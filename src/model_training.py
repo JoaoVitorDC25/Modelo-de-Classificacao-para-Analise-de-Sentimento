1949a5f155eb140b4227f05ee6015680ff38533c
João Vitor Dias <joaovitordiasc.25@gmail.com>
2026-09-02 22:54:06 -0300

    feat: create model_training.py for training model and define hyperparamater

--- src/model_training.py
+++ src/model_training.py
@@ -1,27 +1,65 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
-
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
 
 def buildPipeline() -> Pipeline:
-    """Cria o pipeline com as mesmas etapas e parâmetros do código original."""
-    return Pipeline([
-        ('tfidf', TfidfVectorizer(stop_words=['de', 'a', 'o', 'que', 'e', 'do', 'da', 'em', 'um'])),
-        ('scaler', StandardScaler(with_mean=False)),
-        ('logreg',LogisticRegression(solver='liblinear', random_state=42, max_iter=1000))
-        ])
+     """Cria o pipeline com as mesmas etapas e parâmetros do código original."""
+     return Pipeline([
+         ('tfidf', TfidfVectorizer(stop_words=['de', 'a', 'o', 'que', 'e', 'do', 'da', 'em', 'um'])),
+         ('scaler', StandardScaler(with_mean=False)),
+         ('classifier',LogisticRegression(solver='liblinear', random_state=42, max_iter=1000))
+         ])
 
-def getHyperparameterGrid() -> dict:
-    """Retorna o grid de hiperparâmetros utilizado na otimização."""
-    return {
-        'tfidf__max_features': [500, 1000, 2000],
-        'tfidf__ngram_range': [(1, 1), (1, 2)],
-        'logreg__C': [0.1, 1, 10],
-        'logreg__penalty': ['l1', 'l2'],
-        'logreg__max_iter': [5000, 6000]
-    }
+def getHyperparameterGrid() -> list[dict]:
+    return [
+        {
+            'classifier': [
+                LogisticRegression(
+                    solver='liblinear',
+                    random_state=42
+                )
+            ],
+            'classifier__C': [0.1, 1, 10],
+            'classifier__penalty': ['l1', 'l2'],
+            'classifier__max_iter': [5000, 6000],
+            'tfidf__max_features': [500, 1000, 2000],
+            'tfidf__ngram_range': [(1, 1), (1, 2)]
+        },
+        {
+            'classifier': [
+                KNeighborsClassifier()
+            ],
+            'classifier__n_neighbors': [3, 5, 7],
+            'classifier__weights': ['uniform', 'distance'],
+            'tfidf__max_features': [500, 1000, 2000],
+            'tfidf__ngram_range': [(1, 1), (1, 2)]
+        },
+        {
+            'classifier': [
+                SVC()
+            ],
+            'classifier__C': [0.1, 1, 10],
+            'classifier__kernel': ['linear'],
+            'tfidf__max_features': [500, 1000, 2000],
+            'tfidf__ngram_range': [(1, 1), (1, 2)]
+        },
+        {
+            'classifier': [
+                DecisionTreeClassifier(
+                    random_state=42
+                )
+            ],
+            'classifier__max_depth': [None, 10, 20, 30],
+            'classifier__min_samples_split': [2, 5, 10],
+            'tfidf__max_features': [500, 1000, 2000],
+            'tfidf__ngram_range': [(1, 1), (1, 2)]
+        }
+    ]
 
 def buildGridSearch() -> GridSearchCV:
     """Configura o GridSearchCV preservando a configuração original."""
@@ -33,7 +71,7 @@ def buildGridSearch() -> GridSearchCV:
         parametros_grid,        # Dicionário com as combinações de hiperparâmetros a serem testadas
         cv=5,                   # Número de divisões para validação cruzada (5-fold cross-validation)
         n_jobs=-1,              # Usa todos os núcleos disponíveis do processador para acelerar o processo
-        scoring='accuracy',     # Métrica usada para avaliar o desempenho de cada combinação (aqui, acurácia)
+        scoring='recall',     # Métrica usada para avaliar o desempenho de cada combinação (aqui, acurácia)
         verbose=1               # Nível de detalhamento do output durante a execução (1 exibe progresso básico)
     )
 
@@ -45,4 +83,9 @@ def trainBestModel(gridSearch, X_Train, y_Train):
     print("\nMelhores hiperparâmetros encontrados:\n")
     print(gridSearch.best_params_)
 
+    bestModel = gridSearch.best_estimator_
+    print("\nMelhor modelo treinado com sucesso!\n")
+    print("Resumo do Melhor Modelo: \n")
+    print(bestModel.named_steps['classifier'])
+    
     return gridSearch.best_estimator_
