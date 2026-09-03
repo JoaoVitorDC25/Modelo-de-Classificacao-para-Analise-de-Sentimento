914c914a886c50fbcaf70fff931a25dee216b286
João Vitor Dias <joaovitordiasc.25@gmail.com>
2026-09-02 22:59:47 -0300

    feat: config receives paths for data and model

--- src/config.py
+++ src/config.py
@@ -1,2 +1,17 @@
 #/main.py
-DATA = '../data/dataset.csv'
+DATA = '../data/dataset_ampliado.csv'
+MODEL_PATH = '../models/modelo_sentimento_dsa_v1.joblib'
+
+#/EDA.py
+#Habilita / desabilita a visualização dos dados no terminal
+#INFO_EDA = True
+INFO_EDA = False
+
+#Habilita / desabilita a geração de gráficos
+#GRAPH_EDA = True
+GRAPH_EDA = False
+
+#/model_evaluation.py
+GRAPH_EVALUATE = True
+#GRAPH_EVALUATE = False
+
